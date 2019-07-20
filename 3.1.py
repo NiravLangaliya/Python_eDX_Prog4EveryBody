66c07cf4484641d2f359ecef24f55a4ff50e3cf3
Nirav Langaliya <nirav.langaliya@gmail.com>
2019-07-20 11:22:24 -0700

    Now Both PyCharm and Atom are in sync.

--- 3.1.py
+++ 3.1.py
@@ -3,7 +3,7 @@
 Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours worked above 40 hours.
 Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75).
 You should use input to read a string and float() to convert the string to a number.
-Do not worry about error checking the user input - assume the user types numbers properly.--
+Do not worry about error checking the user input - assume the user types numbers properly.
 """
 
 hrs = input("Enter Hours:")
@@ -18,5 +18,3 @@ else:
     gross_pay = h*rh
 
 print(gross_pay)
-print("Hello")
-print("hidsfds")
